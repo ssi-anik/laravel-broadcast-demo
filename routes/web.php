4a48beb15629364050ee8784b7b6d61900113749
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-05-29 01:45:59 +0600

    login page added

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes([ 'register' => false ]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([ 'middleware' => 'auth' ], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+
+    Route::get('chat/private', function () {
+        return 'not implemented';
+    });
 });
