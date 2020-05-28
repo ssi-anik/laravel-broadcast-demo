b44986cf9854d7df0653d24be83ed403352a1395
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-05-29 03:33:18 +0600

    RouteServiceProvider::HOME variable change

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
